58c38c7a9840f9b0ca49594ee904d5c8a78e8f17
Laura <laura.nunes@labenu.com.br>
2022-10-05 15:19:00 -0300

    feat: structure and migrations

---
+++ backend/src/database/BaseDatabase.ts
@@ -0,0 +1,15 @@
+import knex from "knex"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export abstract class BaseDatabase {
+    protected static connection = knex({
+        client: "pg",
+        connection: {
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        }
+    })
+}
