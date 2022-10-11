400f9d1fd4fdbbdbcce8bfe882d8ac7f22744de3
Laura <laura.nunes@labenu.com.br>
2022-10-11 12:12:21 -0300

    feat: test request

---
+++ frontend/src/constants/urls.jsx
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://kpis-deploy.herokuapp.com/users'
--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+import { BASE_URL } from "../../constants/urls";
+import useRequestData from "../../hooks/useRequestData";
+import { useNavigate } from "react-router-dom";
+import { goToInfos } from "../../routes/coordinator";
 
-function HomeScreen () {
+function HomeScreen() {
+  const [email, setEmail] = useState("");
+  
+  const navigate = useNavigate();
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onSubmitLogin = () => {
+    const body = {
+      email
+    };
+    
+    axios
+      .get(`${BASE_URL}`, body)
+  
+      console.log(axios)
+      .then((response) => {
+        console.log(response.data)
+        goToInfos(navigate);
+      })
+      .catch((error) => {
+        alert(error.response.data);
+      });
+  }
+  
+
   return (
     <div>
-      <h1>Home Screen</h1>
+      <h1>Seu email</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={onChangeEmail}
+        required
+      />
+      <button onClick={onSubmitLogin}>OK</button>
     </div>
-  )
-
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
