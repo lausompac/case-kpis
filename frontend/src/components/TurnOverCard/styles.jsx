68dd0fffbadd0d78383b9b13d8c96e5101479c97
Laura <laura.nunes@labenu.com.br>
2022-10-12 18:21:07 -0300

    styles: components and pages

---
+++ frontend/src/components/TurnOverCard/styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const TurnOverContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 35vw;
+  padding: 5vw 2vw;
+  background-color: #f2f2f2;
+`;
